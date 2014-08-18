7e6e2491418dea0ad81b38553599c4465ea5a082
Tim Spann <tspann@gopivotal.com>
2014-08-18 17:08:29 -0400

    works

--- src/main/java/io/pivotal/nbcuview/Application.java
+++ src/main/java/io/pivotal/nbcuview/Application.java
@@ -1,16 +1,21 @@
-package com.gopivotal.redisstore;
+package io.pivotal.nbcuview;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class) 
-public class Application {
+@EnableAutoConfiguration 
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Override
+    protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
--- src/main/java/io/pivotal/nbcuview/web/DataController.java
+++ src/main/java/io/pivotal/nbcuview/web/DataController.java
@@ -1,57 +1,66 @@
-package com.gopivotal.redisstore.web;
+package io.pivotal.nbcuview.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import io.pivotal.nbcuview.domain.NameValue;
+import io.pivotal.nbcuview.service.DataSourceService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.gopivotal.redisstore.service.DataService;
-import com.gopivotal.redisstore.service.DataSourceService;
-
-@Controller
 @RestController
 public class DataController {
 
-	 Logger logger = LoggerFactory.getLogger(DataController.class);
-	 
-	@Autowired
-	private DataService dataservice;
+	public static HttpServletRequest getCurrentRequest() {
+	     RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+	     Assert.state(requestAttributes != null, "Could not find current request via RequestContextHolder");
+	     Assert.isInstanceOf(ServletRequestAttributes.class, requestAttributes);
+	     HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+	     Assert.state(servletRequest != null, "Could not find current HttpServletRequest");
+	     return servletRequest;
+	 }
+	
+	Logger logger = LoggerFactory.getLogger(DataController.class);
 
 	@Autowired
 	private DataSourceService dataSourceService;
 	
-//	@Autowired
-//	private MessageService messageService;
-	
-    private static final String template = "Key: %s";
-    private static final String templateView = template +  " Value: %s";
+    private static final String templateView = "Name %s Value %s";
 
-    @RequestMapping("/retrieve")
-    public @ResponseBody String retrieve(
-            @RequestParam(value="name", required=false, defaultValue="name") String name) 
+    @RequestMapping("/retrieve/{name}")
+    public NameValue retrieve(
+    		@PathVariable(value="name") String name) 
     {
-    	String value = dataservice.retrieveValue(name);
-    	String returnValue = String.format(templateView, name, value);
-    	logger.info(returnValue);
-    	logger.error("DB RETRIEVE: " + dataSourceService.retrieveValue(name));
+    	NameValue value = dataSourceService.retrieveValue(name);
+    	String returnValue = String.format(templateView, name, value.getValue());
+    	logger.error(returnValue);
+    	final String userIpAddress = getCurrentRequest().getRemoteAddr();
+    	final String userAgent = getCurrentRequest().getHeader("user-agent");
+    	final String userDisplay = String.format("IP:%s Browser:%s", userIpAddress, userAgent);
+    	logger.error(userDisplay);
     	//logger.error(System.getenv("VCAP_SERVICES"));
         return value;
     }
 
-    @RequestMapping("/store")
-    public @ResponseBody String store(
-            @RequestParam(value="name", required=false, defaultValue="name") String name,
-            @RequestParam(value="value", required=false, defaultValue="value") String value) 
+    @RequestMapping("/query/{query}")
+    public List<NameValue> query(
+    		@PathVariable(value="query") String query) 
     {
-    	dataservice.storeValue(name, value);
-    	String returnValue = String.format(template, name);
-    	logger.info(returnValue);
-    	dataSourceService.storeValue(name, value);
-    	return returnValue;
+    	List<NameValue> value = dataSourceService.search(query);
+    	final String userIpAddress = getCurrentRequest().getRemoteAddr();
+    	final String userAgent = getCurrentRequest().getHeader("user-agent");
+    	final String userDisplay = String.format("Query:%s,IP:%s Browser:%s", query, userIpAddress, userAgent);
+    	logger.error(userDisplay);
+        return value;
     }
 }
