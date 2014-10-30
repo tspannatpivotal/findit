0246b0e38d78541c0c55b6d074da352e551ac3d3
tspannatpivotal <tspann@pivotal.io>
2014-10-30 12:39:39 -0400

    update

--- src/main/java/io/pivotal/findit/domain/NameValueRepository.java
+++ src/main/java/io/pivotal/findit/domain/NameValueRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RestController;
 
+@RepositoryRestResource
 public interface NameValueRepository extends CrudRepository<NameValue, Long> {
 
     List<NameValue> findByName(String name);
